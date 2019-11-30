01a6460bfaf3350925e78726ef2a354c73d9aa44
zaidakhterr <zaidakhter1202@gmail.com>
2019-11-30 16:21:29 +0500

    'deploy'

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBBaW5OHuwCxMDRHLsnXGtIXP5IwEcW5U8',
+  authDomain: 'crwnclothing-db.firebaseapp.com',
+  databaseURL: 'https://crwnclothing-db.firebaseio.com',
+  projectId: 'crwnclothing-db',
+  storageBucket: 'crwnclothing-db.appspot.com',
+  messagingSenderId: '282123197374',
+  appId: '1:282123197374:web:1da118b7e09596c94bf7ef',
+  measurementId: 'G-JPGYXKW1QW',
+};
